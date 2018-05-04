a037ff2167aa3b929f601cced853093a95145cd8
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-05-04 14:58:55 -0300

    tests for List ok

--- __tests__/components/List.test.js
+++ __tests__/components/List.test.js
@@ -1,1 +1,28 @@
-test('test', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Text } from 'react-native';
+
+import List from '../../src/List';
+
+const posts = [
+  { id: 1, title: 'Test', description: 'desc' },
+  { id: 2, title: 'Test', description: 'desc' },
+  { id: 3, title: 'Test', description: 'desc' },
+];
+
+describe('<List />', () => {
+  it('renders as expected', () => {
+    const wrapper = shallow(<List posts={posts} />);
+
+    expect(wrapper.children()).toHaveLength(3);
+  });
+
+  it('shows empty message', () => {
+    const wrapper = shallow(<List posts={[]} />);
+    expect(wrapper.contains(<Text>Nenhum post!</Text>)).toBe(true);
+
+    wrapper.setProps({ posts });
+    expect(wrapper.contains(<Text>Nenhum post!</Text>)).toBe(false);
+
+  });
+});
--- src/List.js
+++ src/List.js
@@ -3,12 +3,19 @@ import { View, Text } from 'react-native';
 
 import Post from './Post';
 
-const List = ({ posts }) => {
+const posts = [
+  { id: 1, title: 'Test', description: 'desc' },
+  { id: 2, title: 'Test', description: 'desc' },
+  { id: 3, title: 'Test', description: 'desc' },
+];
+
+const List = ({ posts }) => (
   <View>
     { posts.length > 0
-      ? posts.map(post => <Post key={post.id} post/>)
-      : <Text>Nenhum post!</Text>}
+      ? posts.map(post => <Post key={post.id} post={post}/>)
+      : <Text>Nenhum post!</Text>
+    }
   </View>
-};
+);
 
 export default List;
